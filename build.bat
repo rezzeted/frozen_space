d4f874bcde010f96fc315c82585c55eca0bfd46f
Denis Netahin <denis.netahin@yandex.ru>
2024-10-19 18:41:08 +0300

    add magic_enum

--- build.bat
+++ build.bat
@@ -9,8 +9,7 @@ if not exist "_intermediate_x64\" (
 cd _intermediate_x64
 
 if exist "%ProgramFiles%\Microsoft Visual Studio\2022\" (
-	rem cmake -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH=C:\Qt\6.7.0\msvc2019_64
-	cmake .. -G "Visual Studio 17 2022" -A x64 
+	cmake -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake .. -G "Visual Studio 17 2022" -A x64 
 	goto :finish
 )
 
